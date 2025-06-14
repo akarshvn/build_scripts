ec21eeb36b8a6c9309d0ba4de0207718d4b166a6
akarshvn <akarsh.velan@gmail.com>
2025-06-14 19:50:38 +0530

    scripts: add axionOS build script

---
+++ axion.sh
@@ -0,0 +1,31 @@
+rm -rf .repo/local_manifests/
+
+git clone -b axion https://github.com/akarshvn/local_manifests .repo/local_manifests
+
+echo "----------------------------------"
+echo Local Manifest Cloning Successful
+echo "----------------------------------"
+
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+
+repo sync
+
+echo "----------------"
+echo Sync Successful
+echo "----------------"
+
+rm -rf packages/apps/Updater/
+git clone -b lineage-22.2 https://github.com/akarshvn/android_packages_apps_Updater.git packages/apps/Updater/
+
+source build/envsetup.sh
+
+echo "----------------"
+echo envsetup.sh done
+echo "----------------"
+
+echo "----------------"
+echo Starting Build
+echo "----------------"
+
+axion sweet gms core
+ax -br
