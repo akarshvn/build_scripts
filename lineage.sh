9e07ef591bf38cf20ee0852b717f20c29a165340
akarsh <143282295+akarshvn@users.noreply.github.com>
2025-09-20 11:18:33 +0530

    scripts: interpret escape characters in echo command

--- lineage.sh
+++ lineage.sh
@@ -2,21 +2,21 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs --no-clone-bundle
-echo "\n---------------\nrepo initialised\n---------------\n"
+echo -e "\n---------------\nrepo initialised\n---------------\n"
 
 /opt/crave/resync.sh
-echo "\n---------------\nrepo synced successfully\n---------------\n"
+echo -e "\n---------------\nrepo synced successfully\n---------------\n"
 
 source build/envsetup.sh
-echo "\n---------------\nenvsetup.sh completed\n---------------\n"
+echo -e "\n---------------\nenvsetup.sh completed\n---------------\n"
 
 breakfast sdk_phone_arm64
-echo "\n---------------\nbreakfast completed\n---------------\n"
+echo -e "\n---------------\nbreakfast completed\n---------------\n"
 
-echo "\n---------------\nstart build\n---------------\n"
+echo -e "\n---------------\nstart build\n---------------\n"
 mka
-echo "\n---------------\nbuild ended\n---------------\n"
+echo -e "\n---------------\nbuild ended\n---------------\n"
 
-echo "\n---------------\nexport for avd\n---------------\n"
+echo -e "\n---------------\nexport for avd\n---------------\n"
 mka emu_img_zip
-echo "\n---------------\nexport done\n---------------\n"
+echo -e "\n---------------\nexport done\n---------------\n"
